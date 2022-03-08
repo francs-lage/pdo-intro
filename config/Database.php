d187d52f8f4a69b0b0be294b85bdb71371436b6b
francs <francs.lage@gmail.com>
2022-03-07 18:04:40 -0800

    jawsDB configured

--- config/Database.php
+++ config/Database.php
@@ -1,11 +1,11 @@
 <?php 
-    $host = 'localhost';
-    $db_name = 'world';
-    $username = 'root';
-    //$password = 'pa55word';
+    $host = 's29oj5odr85rij2o.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    $db_name = 'saue4p4vl4n7s6v2';
+    $username = 'cicnlgrlezlr3gvu';
+    $password = 'niar77j0vassnkzj';
 
     try {
-        $db = new PDO("mysql:host={$host};dbname={$db_name}", $username); //, $password
+        $db = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
         $error_message = 'Database Error: ';
